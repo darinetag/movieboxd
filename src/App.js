701d96937098aacf382b02517b03e9025241a665
Darine <tagdarinefatma@gmail.com>
2024-02-12 16:26:34 +0100

    useState Counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+// practice counter (useState)
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <button onClick={() => setCounter((prevcount) => prevcount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevcount) => prevcount + 1)}>
+        +
+      </button>
     </div>
   );
-}
+};
 
 export default App;
