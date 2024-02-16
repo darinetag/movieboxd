bfcb752ad0abcc8d393de1c0250fa5074874d00d
Darine <tagdarinefatma@gmail.com>
2024-02-16 22:52:41 +0100

    initialize movie project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import { useState } from "react";
 import "./App.css";
-// practice counter (useState)
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevcount) => prevcount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevcount) => prevcount + 1)}>
-        +
-      </button>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
